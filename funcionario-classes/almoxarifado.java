2076f8ee328b75a21171c24f0bdce6d544c6f863
Gal Levy <gallevy@edu.unifil.br>
2020-09-23 17:25:31 -0300

    Add files via upload

---
+++ funcionario-classes/almoxarifado.java
@@ -0,0 +1,5 @@
+public class almoxarifado extends funcionario{
+    public almoxarifado(String senha, String email){
+        super(senha, email);
+    }
+}
---
+++ funcionario-classes/tecnicoAA.java
@@ -0,0 +1,5 @@
+public class tecnicoAA extends funcionario{
+    public tecnicoAA(String senha, String email){
+        super(senha, email);
+    }
+}
